c7251601330187217d056f257b47486f1b274bb7
Paweł Szczerbiak <p.szczerbiak@gmail.com>
2018-02-03 11:15:13 +0100

    First commit

---
+++ src/main/java/pl/szczerbiak/kwejk/controller/HomeController.java
@@ -0,0 +1,13 @@
+package pl.szczerbiak.kwejk.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String hello(){
+        return "hello";
+    }
+}
