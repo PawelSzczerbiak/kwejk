95133706d9cc9be96cf38e9cbd2bab6731de9c30
Paweł Szczerbiak <p.szczerbiak@gmail.com>
2018-02-12 14:44:27 +0100

    Files' upload functionality added

--- src/main/java/pl/szczerbiak/kwejk/KwejkApplication.java
+++ src/main/java/pl/szczerbiak/kwejk/KwejkApplication.java
@@ -1,12 +1,28 @@
 package pl.szczerbiak.kwejk;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import  pl.szczerbiak.kwejk.storage.StorageProperties;
+import  pl.szczerbiak.kwejk.storage.StorageService;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class KwejkApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KwejkApplication.class, args);
 	}
+
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
+
 }
---
+++ src/main/java/pl/szczerbiak/kwejk/storage/StorageFileNotFoundException.java
@@ -0,0 +1,12 @@
+package pl.szczerbiak.kwejk.storage;
+
+public class StorageFileNotFoundException extends StorageException {
+
+    public StorageFileNotFoundException(String message) {
+        super(message);
+    }
+
+    public StorageFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
