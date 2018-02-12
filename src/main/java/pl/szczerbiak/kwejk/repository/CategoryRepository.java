b4dda30cff6a22247ae09301b262fb851d4ffcb1
Paweł Szczerbiak <p.szczerbiak@gmail.com>
2018-02-12 19:33:00 +0100

    Little changes, gifs from a given category displayed

--- src/main/java/pl/szczerbiak/kwejk/repository/CategoryRepository.java
+++ src/main/java/pl/szczerbiak/kwejk/repository/CategoryRepository.java
@@ -1,9 +1,12 @@
 package pl.szczerbiak.kwejk.repository;
 
 import pl.szczerbiak.kwejk.model.Category;
+import pl.szczerbiak.kwejk.model.Gif;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CategoryRepository {
 
@@ -12,7 +15,13 @@ public class CategoryRepository {
     static {
         ALL_CATEGORIES.add(new Category(1L, "Programming"));
         ALL_CATEGORIES.add(new Category(2L, "Fun"));
-        ALL_CATEGORIES.add(new Category(2L, "Other"));
+        ALL_CATEGORIES.add(new Category(3L, "Other"));
+    }
+
+    public static Category findByCategoryId(int categoryId){
+        return ALL_CATEGORIES.stream()
+                .filter(category -> (category.getId() == categoryId))
+                .collect(Collectors.toList()).get(0);
     }
 
     public static List<Category> findAll() {
