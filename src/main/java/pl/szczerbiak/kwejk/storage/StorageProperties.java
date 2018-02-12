dfb1901ae677f79effc70403557fa9f3d49680f2
Paweł Szczerbiak <p.szczerbiak@gmail.com>
2018-02-12 15:40:05 +0100

    Upload directory changed

--- src/main/java/pl/szczerbiak/kwejk/storage/StorageProperties.java
+++ src/main/java/pl/szczerbiak/kwejk/storage/StorageProperties.java
@@ -8,7 +8,7 @@ public class StorageProperties {
     /**
      * Folder location for storing files
      */
-    private String location = "upload-dir";
+    private String location = "src/main/resources/static/gifs-uploaded";
 
     public String getLocation() {
         return location;
