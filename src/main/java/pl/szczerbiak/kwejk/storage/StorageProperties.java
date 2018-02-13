7303bfaac7efe360f990e066e0afc36826747d78
Paweł Szczerbiak <p.szczerbiak@gmail.com>
2018-02-13 13:40:27 +0100

    Server relative URL for uploaded files added

--- src/main/java/pl/szczerbiak/kwejk/storage/StorageProperties.java
+++ src/main/java/pl/szczerbiak/kwejk/storage/StorageProperties.java
@@ -8,7 +8,7 @@ public class StorageProperties {
     /**
      * Folder location for storing files
      */
-    private static String location = "src/main/resources/static/gifs-uploaded";
+    private static String location = "upload/files";
 
     public static String getLocation() {
         return location;
